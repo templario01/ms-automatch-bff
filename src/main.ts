d581f069d07582b96d4d525a9f21d31061c7794d
Victor Manuel Benavente Yepez <88349543+victorbenavente@users.noreply.github.com>
2024-01-30 10:59:19 -0500

    feat: add fastify and cicd

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const port = app.get(ConfigService).get<number>('PORT');
   const logger = new Logger('Bootstrap');
 
-  await app.listen(port, '0.0.0.0', () => {
+  await app.listen(port, (_err, address) => {
     logger.log(`Server running on port: ${port} 🚀 ✨✨`);
+    logger.log(`GraphQL URL: ${address}/graphql`);
   });
 }
 bootstrap();
