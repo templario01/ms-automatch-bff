cb1467acf5e737d72ffd004df7ca8f91119bd6fe
Victor Manuel Benavente Yepez <88349543+victorbenavente@users.noreply.github.com>
2024-04-10 20:47:29 -0500

    refactor: update tracking interceptor

--- src/common/interceptors/tracking-search.interceptor.ts
+++ src/common/interceptors/tracking-search.interceptor.ts
@@ -5,8 +5,8 @@ import {
   CallHandler,
   Logger,
 } from '@nestjs/common';
-import { Observable, from } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { EMPTY, Observable, from } from 'rxjs';
+import { catchError, switchMap, tap } from 'rxjs/operators';
 import { RedisClient } from '../../settings/redis/redis.client';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { SearchVehiclesInput } from '../../vehicles/graphql-types/inputs/search-vehicles.input';
@@ -20,37 +20,45 @@ export class TrackingSearchInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const gqlContext = GqlExecutionContext.create(context);
     const request = gqlContext.getContext().req;
-
     const search = request.body.variables.searchVehiclesInput;
-    console.log(request.body.operationName);
     const key = Buffer.from(request.body.operationName).toString('base64');
 
-    return next.handle().pipe(tap(from(this.saveInRedis(key, search))));
+    return next.handle().pipe(
+      tap(
+        this.saveInRedis(key, search).pipe(
+          tap(() => this.logger.debug('new search saved in Redis')),
+          catchError((error) => {
+            this.logger.error('Error in saveInRedis:', error);
+            return EMPTY;
+          }),
+        ),
+      ),
+    );
   }
 
-  private async saveInRedis(
+  private saveInRedis(
     key: string,
     newSearch: SearchVehiclesInput,
-  ): Promise<void> {
-    try {
-      const result = await this.redisClient.adapter.get(key);
-      let data = [];
-      if (result) {
-        const searches = JSON.parse(result);
-        data = searches.data;
-        if (data.length >= 100) {
-          data = [];
+  ): Observable<void> {
+    return from(this.redisClient.adapter.get(key)).pipe(
+      switchMap((result) => {
+        let data = [];
+        if (result) {
+          const searches = JSON.parse(result);
+          data = searches.data;
+          if (data.length >= 100) {
+            data = [];
+          }
         }
-      }
-      const newSearches = JSON.stringify({
-        data: [...data, newSearch],
-      });
-      await this.redisClient.adapter.set(key, newSearches, {
-        ttl: this.ONE_MONTH_TTL,
-      });
-      this.logger.debug('new search saved in Redis');
-    } catch (error) {
-      this.logger.error('fail to save search in Redis');
-    }
+        const newSearches = JSON.stringify({
+          data: [...data, newSearch],
+        });
+        return from(
+          this.redisClient.adapter.set(key, newSearches, {
+            ttl: this.ONE_MONTH_TTL,
+          }),
+        );
+      }),
+    );
   }
 }
