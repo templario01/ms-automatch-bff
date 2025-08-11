32d8aafcd0556e2c533280a8ef589ae75b0b2e8c
victor benavente <templario0141@gmail.com>
2025-08-11 18:48:26 -0500

    fix: fix broken dependencies

--- src/settings/redis/redis.client.ts
+++ src/settings/redis/redis.client.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { EnvConfigService } from '../config/env-config.service';
-import * as Keyv from 'keyv';
+import { Keyv } from 'keyv';
+import KeyvRedis from '@keyv/redis';
 import { KeyvAdapter } from '@apollo/utils.keyvadapter';
 import KeyvGzip from '@keyv/compress-gzip';
 
@@ -11,9 +12,7 @@ export class RedisClient {
   private readonly logger = new Logger(RedisClient.name);
   constructor(private readonly envConfig: EnvConfigService) {
     const { url } = this.envConfig.redis;
-    this.redis = new Keyv(url, {
-      compresion: KeyvGzip,
-    });
+    this.redis = new Keyv(new KeyvRedis(url), { compression: KeyvGzip });
     this.redis.on('error', () => {
       this.logger.error('Fail to connect with Redis');
     });
