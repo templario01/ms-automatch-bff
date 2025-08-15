c1d33ee66b0bffd1cdb38b7f83a9836694570c4a
victor benavente <templario0141@gmail.com>
2025-08-14 19:55:17 -0500

    fix: decrease redis error

--- src/settings/redis/redis.client.ts
+++ src/settings/redis/redis.client.ts
@@ -10,11 +10,16 @@ export class RedisClient {
   private redis: Keyv;
   private keyvAdapter: KeyvAdapter;
   private readonly logger = new Logger(RedisClient.name);
+  private lastErrorLogged = 0;
   constructor(private readonly envConfig: EnvConfigService) {
     const { url } = this.envConfig.redis;
     this.redis = new Keyv(new KeyvRedis(url), { compression: KeyvGzip });
     this.redis.on('error', () => {
-      this.logger.error('Fail to connect with Redis');
+      const now = Date.now();
+      if (now - this.lastErrorLogged > 60000) {
+        this.lastErrorLogged = now;
+        this.logger.error('Fail to connect with Redis');
+      }
     });
 
     this.keyvAdapter = new KeyvAdapter(this.redis);
