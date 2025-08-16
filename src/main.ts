ea5fdffe9f0051b1736e7276d530021db85aed64
victor benavente <templario0141@gmail.com>
2025-08-16 01:20:18 -0500

    chore: add prefix for ingress

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+  app.setGlobalPrefix('ms-automatch-bff');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
